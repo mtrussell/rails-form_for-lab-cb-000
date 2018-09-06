721213f22aade0b04e20d4f31537f03a5b3c3dbb
Matt Trussell <smtrussell@gmail.com>
2018-09-06 02:49:17 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,28 @@
 class StudentsController < ApplicationController
 
+  def index
+    @students = Students.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = 
+  end
+
+  def edit
+  end
+
+  def update
+  end
 
 
   private
 
-  def students_params(*args)
+  def student_params(*args)
     params.require(:student).permit(*args)
   end
 end
