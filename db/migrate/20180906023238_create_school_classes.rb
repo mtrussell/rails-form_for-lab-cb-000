30cb4a44e02d9a445316eef5566cc08081f991cf
Matt Trussell <smtrussell@gmail.com>
2018-09-06 02:43:20 +0000

    automatically backed up by learn

--- db/migrate/20180906023238_create_school_classes.rb
+++ db/migrate/20180906023238_create_school_classes.rb
@@ -3,6 +3,7 @@ class CreateSchoolClasses < ActiveRecord::Migration
     create_table :school_classes do |t|
       t.string :title
       t.integer :room_number
+      
       t.timestamps null: false
     end
   end
