e19a5346d0e7a4101c577282e027c6e86f10d9b0
Matt Trussell <smtrussell@gmail.com>
2018-09-06 03:02:33 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,6 +1,33 @@
 class SchoolClassesController < ApplicationController
 
+  def index
+    @school_classes = SchoolClass.all
+  end
 
+  def show
+    @school_class = SchoolClass.find(:id)
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path
+  end
+
+  def edit
+    @school_class = SchoolClass.find(:id)
+  end
+
+  def update
+    @school_class = SchoolClass.find(:id)
+    @school_class.update(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path
+  end
 
   private
 
